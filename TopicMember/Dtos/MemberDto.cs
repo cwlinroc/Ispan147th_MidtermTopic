d97ff2bbc82da5a51ee65dd40409c5ca8032ccb0
lin274606 <lin274606@gmail.com>
2023-05-19 18:07:27 +0800

    0519

---
+++ TopicMember/Dtos/MemberDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberClassLibrary.Dtos
+{
+	public class MemberDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime Dateofbirth { get; set; }
+		public bool Gender { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+		public string Email { get; set; }
+	}
+}
