5a94a4ae76da1cd0abc4bbf18900e2766fe16868
cwlinroc <129026699+cwlinroc@users.noreply.github.com>
2023-05-21 22:52:51 +0800

    2023.5.21  merge try

--- Ispan147.Estore.SqlDataLayer/Properties/AssemblyInfo.cs
+++ Ispan147.Estore.SqlDataLayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("Ispan147.Estore.SqlDataLayer")]
+[assembly: AssemblyTitle("ISpan147.Estore.SqlDataLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ispan147.Estore.SqlDataLayer")]
+[assembly: AssemblyProduct("ISpan147.Estore.SqlDataLayer")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("0ec3bab8-1ccc-430b-98a1-30c2d84d4808")]
+[assembly: Guid("9ee28126-8c44-4f99-867d-e316d537541b")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
